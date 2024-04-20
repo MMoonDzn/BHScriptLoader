b0941151a30943ce58dd6604fb8e9be802142daa
MMoon <108307939+MMoonDzn@users.noreply.github.com>
2024-04-20 10:18:24 -0300

    fixed issues

--- ByMMoonDzn/Source.lua
+++ ByMMoonDzn/Source.lua
@@ -10,22 +10,6 @@ Text = "By: mmoondzn | MMoonZim";
 Duration = 5; 
 })
 wait(2)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JulHubz/JulHub/main/JulHub"))()
-wait()
-game.StarterGui:SetCore("SendNotification", {
-Title = "JulHub Loaded"; 
-Text = "By: JulHubz"; 
-Duration = 3; 
-})
-wait()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-wait()
-game.StarterGui:SetCore("SendNotification", {
-Title = "Infinite Yield"; 
-Text = "By: EdgeIY"; 
-Duration = 3;
-})
-wait()
 local IsDevelopmentBranch,NotificationTime = false,5
 local Branch = IsDevelopmentBranch and "development" or "main"
 local Source = "https://raw.githubusercontent.com/AlexR32/Parvus/" .. Branch .. "/"
@@ -35,4 +19,20 @@ game.StarterGui:SetCore("SendNotification", {
 Title = "ESP"; 
 Text = "By: AlexR32"; 
 Duration = 3;
+})
+wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+wait()
+game.StarterGui:SetCore("SendNotification", {
+Title = "Infinite Yield"; 
+Text = "By: EdgeIY"; 
+Duration = 3;
+})
+wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JulHubz/JulHub/main/JulHub"))()
+wait()
+game.StarterGui:SetCore("SendNotification", {
+Title = "JulHub Loaded"; 
+Text = "By: JulHubz"; 
+Duration = 3; 
 })
