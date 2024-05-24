be95986303b7c5e71ec7e3fb74337aea9696a410
MMoon <108307939+MMoonDzn@users.noreply.github.com>
2024-05-23 22:54:59 -0300

    Add animations script

--- ByMMoonDzn/Source.lua
+++ ByMMoonDzn/Source.lua
@@ -9,9 +9,11 @@ Title = "Brookhaven Loader";
 Text = "By: mmoondzn | MMoonZim"; 
 Duration = 5; 
 })
-wait(1)
+wait()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/fuckusfm/infiniteyield-reborn/master/source'))()
 wait()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/REDzHUB/main/REDzHUB"))()
 wait()
 pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua'))() end)
+wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Eazvy/public-scripts/main/Universal_Animations_Emotes.lua"))()
